53863a531fb9c8dcf8276da728ad8d547bdd0966
Yury Kozyrev <urakozz@me.com>
2014-12-23 17:01:51 +0300

    Update Controller.php

--- app/Controller.php
+++ app/Controller.php
@@ -1,36 +1,9 @@
 <?php
-namespace MVC
+namespace App
 
-abstract class Controller {
-    /** @var  View */
-    protected $_view;
-    /** @var  Model */
-    protected $_model;
+class MainController extends Controller {
     
-    function __construct (Model $model, View $view)
-    {
-        $this->_model = $model;
-        $this->_view  = $view;
-    }
-    
-    function action ()
-    {
-        $this->_view->generate($this->_model->getData());
-    }
-    
-    /**
-     * @return Model
-     */
-    public function getView ()
-    {
-        return $this->_view;
-    }
-    
-    /**
-     * @return View
-     */
-    public function getModel ()
-    {
-        return $this->_model;
+    public function action(){
+        
     }
 }
